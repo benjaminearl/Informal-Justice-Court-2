fc295d5a8196cfc32dcb0bb6389905fc514d36d3
benjaminearl <bnjmnearl@gmail.com>
2020-05-05 12:14:21 +0200

    add image to introduction

--- web/_data/metadata.js
+++ web/_data/metadata.js
@@ -3,6 +3,7 @@ const client = require('../utils/sanityClient')
 module.exports =  async function() {
   return await client.fetch(groq`
     *[_id == "siteSettings"]{
+      "imageUrl": mainImage.asset->url,
       ...,
       teamMember->
     }[0]
