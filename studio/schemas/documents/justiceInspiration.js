3949347d7278e36a0ce4b251dfa6f5ce50e15dc6
benjaminearl <bnjmnearl@gmail.com>
2020-06-15 11:04:30 +0200

    add instructions to cms

--- studio/schemas/documents/justiceInspiration.js
+++ studio/schemas/documents/justiceInspiration.js
@@ -9,16 +9,20 @@ export default {
       name: 'title',
       type: 'string',
       title: 'Title',
+      description: 'Title of page. This will appear on the homepage with an image'
     },
     {
       name: 'subtitle',
       type: 'string',
-      title: 'Subtitle'
+      title: 'Subtitle',
+      description: 'Subtitle of the page. This will appear on the page of the content itself.'
     },
     {
       name: 'slug',
       type: 'slug',
       title: 'Slug',
+      description: 'The slug creates the URL of the page.',
+      validation: Rule => Rule.error('You need to generate a slug for the page to appear.').required(),
       options: {
         source: 'title',
         maxLength: 96
@@ -27,17 +31,20 @@ export default {
     {
       name: 'mainImage',
       title: 'Thumbnail Image',
-      type: 'mainImage'
+      type: 'mainImage',
+      description: 'This image will act as the thumbnail image and cover image for the content page'
     },
     {
       name: 'excerpt',
       title: 'Excerpt',
-      type: 'excerptPortableText'
+      type: 'excerptPortableText',
+      description: 'The excerpt will be featured on the carousel if this content is chosen to appear. One sentance max.'
     },
     {
       name: 'body',
       type: 'bodyPortableText',
-      title: 'Body'
+      title: 'Body',
+      description: 'This is the main content of the page you are adding. Images and text can be added.'
     }
   ]
 }
